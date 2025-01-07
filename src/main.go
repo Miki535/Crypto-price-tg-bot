b360eef62fcbb93bc13a09450e74a5cb28e93091
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2025-01-07 22:15:39 +0200

    Basic Setup
    Basic Setup from official documentation.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,26 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/mymmrac/telego"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	botToken := os.Getenv("TOKEN")
+
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	botUser, err := bot.GetMe()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Printf("Bot user: %+v\n", botUser)
 }
